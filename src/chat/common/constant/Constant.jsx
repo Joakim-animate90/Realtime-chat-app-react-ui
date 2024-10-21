50b8dfa1b7c51589def2b6d11ce235550b044cea
Joakim-animate90 <joakimbwire23@gmail.com>
2024-10-21 12:20:19 +0300

    Add constants for online voice and video chat

---
+++ src/chat/common/constant/Constant.jsx
@@ -0,0 +1,19 @@
+export const AUDIO_ONLINE = 6; // Voice chat
+export const VIDEO_ONLINE = 7; // video chat
+
+export const DIAL_MEDIA_START = 10; // Dial the media to start the placement symbol
+export const DIAL_AUDIO_ONLINE = 11; // Voice chat dial
+export const ACCEPT_AUDIO_ONLINE = 12; // Voice chat answering
+export const CANCELL_AUDIO_ONLINE = 13; // Voice chat cancel
+export const REJECT_AUDIO_ONLINE = 14; // Voice chat refusal
+
+export const DIAL_VIDEO_ONLINE = 15; // Video chat dial
+export const ACCEPT_VIDEO_ONLINE = 16; // Video chat answering
+export const CANCELL_VIDEO_ONLINE = 17; 
+export const REJECT_VIDEO_ONLINE = 18; 
+
+export const DIAL_MEDIA_END = 20; 
+
+
+export const MESSAGE_TRANS_TYPE = "webrtc"; // Message transmission type: If it is a heartbeat message, this content is heatbeat, online video or audio is webrtc
+
