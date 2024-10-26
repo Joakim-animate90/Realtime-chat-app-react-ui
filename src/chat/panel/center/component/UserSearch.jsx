58bceaff4ab8b825f3c3dc48baa5ca0cc2728d2e
Joakim-animate90 <joakimbwire23@gmail.com>
2024-10-26 18:43:40 +0300

    Refactor Docker-compose.yml and UserSearch component

--- src/chat/panel/center/component/UserSearch.jsx
+++ src/chat/panel/center/component/UserSearch.jsx
@@ -11,12 +11,10 @@ import {
     message
 } from 'antd';
 import { PlusCircleOutlined } from '@ant-design/icons';
-
-import { connect } from 'react-redux'
-import { actions } from '../../../redux/module/panel'
-import * as Params from '../../../common/param/Params'
+import { connect } from 'react-redux';
+import { actions } from '../../../redux/module/panel';
+import * as Params from '../../../common/param/Params';
 import { axiosGet, axiosPostBody } from '../../../util/Request';
-
 
 class UserSearch extends React.Component {
     groupForm = React.createRef();
@@ -32,162 +30,145 @@ class UserSearch extends React.Component {
         }
     }
 
-    componentDidMount() {
-
-    }
-
-    /**
-     * Search user
-     * @param {*} value 
-     * @param {*} _event 
-     * @returns 
-     */
     searchUser = (value, _event) => {
-        if (null === value || "" === value) {
-            return
-        }
+        if (!value) return;
 
-        let data = {
-            name: value
-        }
+        const data = { name: value };
         axiosGet(Params.USER_NAME_URL, data)
             .then(response => {
-                let data = response.data
-                if (data.user.username === "" && data.group.name === "") {
-                    message.error("No group or user")
-                    return
+                const data = response.data;
+                if (!data.user.username && !data.group.name) {
+                    message.error("No group or user found");
+                    return;
                 }
-                let queryUser = {
+                const queryUser = {
                     username: data.user.username,
                     nickname: data.user.nickname,
-
                     groupUuid: data.group.uuid,
                     groupName: data.group.name,
-                }
-                this.setState({
-                    hasUser: true,
-                    queryUser: queryUser
-                });
+                };
+                this.setState({ hasUser: true, queryUser });
             });
     }
 
-    showModal = () => {
-        this.setState({
-            hasUser: true
-        });
-    };
-
+    showModal = () => this.setState({ hasUser: true });
+    
     addUser = () => {
-        let data = {
+        const data = {
             uuid: localStorage.uuid,
             friendUsername: this.state.queryUser.username
-        }
+        };
         axiosPostBody(Params.USER_FRIEND_URL, data)
-            .then(_response => {
-                message.success("Succeed")
-                // this.fetchUserList()
-                this.setState({
-                    hasUser: false
-                });
+            .then(() => {
+                message.success("User added successfully");
+                this.setState({ hasUser: false });
             });
     };
 
     joinGroup = () => {
-        // /group/join/:userUid/:groupUuid
-        axiosPostBody(Params.GROUP_JOIN_URL + localStorage.uuid + "/" + this.state.queryUser.groupUuid)
-            .then(_response => {
-                message.success("Succeed")
-                // this.fetchUserList()
-                this.setState({
-                    hasUser: false
-                });
+        axiosPostBody(`${Params.GROUP_JOIN_URL}${localStorage.uuid}/${this.state.queryUser.groupUuid}`)
+            .then(() => {
+                message.success("Joined group successfully");
+                this.setState({ hasUser: false });
             });
     }
 
-    handleCancel = () => {
-        this.setState({
-            hasUser: false
-        });
-    };
+    handleCancel = () => this.setState({ hasUser: false });
+    showCreateGroup = () => this.setState({ showCreateGroup: true });
+    handleCancelGroup = () => this.setState({ showCreateGroup: false });
 
-    showCreateGroup = () => {
-        this.setState({
-            showCreateGroup: true
-        });
-    }
-
-    handleCancelGroup = () => {
-        this.setState({
-            showCreateGroup: false
-        });
-    }
-
-    /**
-     * Creation group
-     */
     createGroup = () => {
-        console.log(this.groupForm.current.getFieldValue())
-        let values = this.groupForm.current.getFieldValue();
-        let data = {
-            name: values.groupName
-        }
-
-        axiosPostBody(Params.GROUP_LIST_URL + "/" + localStorage.uuid, data)
-            .then(_response => {
-                message.success("Succeed")
-                this.setState({
-                    showCreateGroup: false
-                });
+        const values = this.groupForm.current.getFieldValue();
+        const data = { name: values.groupName };
+        axiosPostBody(`${Params.GROUP_LIST_URL}/${localStorage.uuid}`, data)
+            .then(() => {
+                message.success("Group created successfully");
+                this.setState({ showCreateGroup: false });
             });
     }
 
     render() {
         const menu = (
             <Menu>
-                <Menu.Item key={1}>
-                    <Button type='link' onClick={this.showModal}>Add users</Button>
+                <Menu.Item key="1">
+                    <Button type='link' onClick={this.showModal}>Add User</Button>
                 </Menu.Item>
-                <Menu.Item key={2}>
-                    <Button type='link' onClick={this.showModal}>Additional group</Button>
+                <Menu.Item key="2">
+                    <Button type='link' onClick={this.showModal}>Add to Group</Button>
                 </Menu.Item>
-                <Menu.Item key={3}>
-                    <Button type='link' onClick={this.showCreateGroup}>Creation group</Button>
+                <Menu.Item key="3">
+                    <Button type='link' onClick={this.showCreateGroup}>Create Group</Button>
                 </Menu.Item>
             </Menu>
         );
 
         return (
             <>
-                <Row>
-                    <Col span={20} >
+                <Row style={{ marginBottom: 16 }}>
+                    <Col span={20}>
                         <Input.Group compact>
-                            <Input.Search allowClear style={{ width: '100%' }} onSearch={this.searchUser} />
+                            <Input.Search
+                                allowClear
+                                style={{ width: '100%', borderColor: '#52c41a' }}
+                                onSearch={this.searchUser}
+                                placeholder="Search by username"
+                            />
                         </Input.Group>
                     </Col>
                     <Col>
                         <Dropdown overlay={menu} placement="bottomCenter" arrow>
-                            <PlusCircleOutlined style={{ fontSize: 22, color: 'gray', marginLeft: 3, marginTop: 5 }} />
+                            <PlusCircleOutlined style={{ fontSize: 24, color: '#52c41a', marginLeft: 8 }} />
                         </Dropdown>
                     </Col>
                 </Row>
 
-
-                <Modal title="User information" visible={this.state.hasUser} onCancel={this.handleCancel} okText="Add users" footer={null}>
+                <Modal
+                    title="User Information"
+                    visible={this.state.hasUser}
+                    onCancel={this.handleCancel}
+                    footer={null}
+                >
                     <Input.Group compact>
-                        <Input.Search allowClear style={{ width: '100%' }} onSearch={this.searchUser} />
+                        <Input.Search
+                            allowClear
+                            style={{ width: '100%', borderColor: '#52c41a' }}
+                            onSearch={this.searchUser}
+                            placeholder="Search for another user"
+                        />
                     </Input.Group>
-                    <br /><hr /><br />
-
-                    <p>username：{this.state.queryUser.username}</p>
-                    <p>Nick name：{this.state.queryUser.nickname}</p>
-                    <Button type='primary' onClick={this.addUser} disabled={this.state.queryUser.username == null || this.state.queryUser.username === ''}>添加用户</Button>
-                    <br /><br /><hr /><br /><br />
-
-                    <p>Group information：{this.state.queryUser.groupName}</p>
-                    <Button type='primary' onClick={this.joinGroup} disabled={this.state.queryUser.groupUuid == null || this.state.queryUser.groupUuid === ''}>添加群</Button>
+                    <div style={{ marginTop: 16 }}>
+                        <p><strong>Username:</strong> {this.state.queryUser.username}</p>
+                        <p><strong>Nickname:</strong> {this.state.queryUser.nickname}</p>
+                        <Button
+                            type='primary'
+                            style={{ backgroundColor: '#52c41a', borderColor: '#52c41a', color: 'white' }}
+                            onClick={this.addUser}
+                            disabled={!this.state.queryUser.username}
+                        >
+                            Add User
+                        </Button>
+                    </div>
+                    <div style={{ marginTop: 16, borderTop: '1px solid #f0f0f0', paddingTop: 16 }}>
+                        <p><strong>Group:</strong> {this.state.queryUser.groupName}</p>
+                        <Button
+                            type='primary'
+                            style={{ backgroundColor: '#52c41a', borderColor: '#52c41a' ,color: 'white'}}
+                            onClick={this.joinGroup}
+                            disabled={!this.state.queryUser.groupUuid}
+                        >
+                            Join Group
+                        </Button>
+                    </div>
                 </Modal>
 
-                <Modal title="Creation group" visible={this.state.showCreateGroup} onCancel={this.handleCancelGroup} onOk={this.createGroup} okText="创建群">
+                <Modal
+                    title="Create Group"
+                    visible={this.state.showCreateGroup}
+                    onCancel={this.handleCancelGroup}
+                    onOk={this.createGroup}
+                    okText="Create"
+                    okButtonProps={{ style: { backgroundColor: '#52c41a', borderColor: '#52c41a' } }}
+                >
                     <Form
                         name="groupForm"
                         ref={this.groupForm}
@@ -196,32 +177,26 @@ class UserSearch extends React.Component {
                     >
                         <Form.Item
                             name="groupName"
-                            label="Group name"
-                            rules={[{ required: true }]}
+                            label="Group Name"
+                            rules={[{ required: true, message: 'Please enter a group name' }]}
                         >
-                            <Input placeholder="Group name" />
+                            <Input placeholder="Enter group name" />
                         </Form.Item>
                     </Form>
-
                 </Modal>
             </>
         );
     }
 }
 
+const mapStateToProps = (state) => ({
+    user: state.userInfoReducer.user,
+});
 
-function mapStateToProps(state) {
-    return {
-        user: state.userInfoReducer.user,
-    }
-}
+const mapDispatchToProps = (dispatch) => ({
+    setUser: (data) => dispatch(actions.setUser(data)),
+});
 
-function mapDispatchToProps(dispatch) {
-    return {
-        setUser: (data) => dispatch(actions.setUser(data)),
-    }
-}
+export default connect(mapStateToProps, mapDispatchToProps)(UserSearch);
 
-UserSearch = connect(mapStateToProps, mapDispatchToProps)(UserSearch)
 
-export default UserSearch
